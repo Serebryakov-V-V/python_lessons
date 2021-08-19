03bb4e4952d1e27e5f702838741197d2f311a55f
Serebryakov-V-V <pro4web@yandex.ru>
2021-08-19 12:46:21 +0500

    more lesson features

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -1,7 +1,6 @@
 from time import time
 from functools import wraps
 
-
 # constants
 NUM_TYPE_EVEN = 'even'
 NUM_TYPE_ODD = 'odd'
@@ -44,33 +43,51 @@ print(dicts['spamm'])
 
 
 # Decoratot function
-def leadTimeDecor(func):
+def run_time_decor(func):
+    print('In decorator')
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print('In decorated function')
+        stat = time()
+        result = func(*args, **kwargs)
+        end = time()
+        time_diff = end - stat
+        print(f"time diff: {time_diff:.15f}")
+        return result
+
+    print('In outer decorated function')
+    return wrapper
 
 
 # Function of exponentiation
-def degreeUp(nums, factor=2):
-    numDict = {}
+@run_time_decor
+def degree_up(nums, factor=4):
+    num_dict = {}
     for n in nums:
-        numDict[n] = n ** factor
-    return numDict
+        num_dict[n] = n ** factor
+    return num_dict
 
 
-print(degreeUp((1, 2, 3, 4, 5, 6, 7, 8)))
+print(degree_up((1, 2, 3, 4, 5, 6, 7, 8)))
+print('Not decorated degree_up: ' ,degree_up.__wrapped__((1,2,3)))
 
-
-# Nums auto-filter functions
+# Nums auto-filter functions in range
+@run_time_decor
 def numsFilter(nums):
-    numDict = {}
-    for i, n in enumerate(nums):
+    num_dict = {}
+    for i, n in enumerate(range(nums)):
+        if n == 0:
+            continue
         if n % 2 == 0:
             key = 'ev-' + str(i)
-            numDict[key] = n
+            num_dict[key] = n
         if n % 2 > 0:
             key = 'od-' + str(i)
-            numDict[key] = n
+            num_dict[key] = n
         if n == 2:
             key = 'si-' + str(i)
-            numDict[key] = n
+            num_dict[key] = n
         if n != 2:
             d = 2
             flag = True
@@ -81,22 +98,25 @@ def numsFilter(nums):
                 d += 1
             if flag:
                 key = 'si-' + str(i)
-                numDict[key] = n
-    return numDict
+                num_dict[key] = n
+    return num_dict
 
-print(numsFilter((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)))
+
+print(numsFilter((30)))
+
 
 # Nums arg-filter functions
+@run_time_decor
 def numsFilterArgs(nums, type=NUM_TYPE_EVEN):
-    numList = []
+    num_list = []
     for n in nums:
         if (type == 'odd') and (n % 2 > 0):
-            numList.append(n)
+            num_list.append(n)
         if (type == 'even') and (n % 2 == 0):
-            numList.append(n)
+            num_list.append(n)
         if (type == 'simple'):
             if n == 2:
-                numList.append(n)
+                num_list.append(n)
             if n != 2:
                 d = 2
                 flag = True
@@ -106,10 +126,49 @@ def numsFilterArgs(nums, type=NUM_TYPE_EVEN):
                         break
                     d += 1
                 if flag:
-                    numList.append(n)
+                    num_list.append(n)
 
-    return numList
+    return num_list
+
 
 print(numsFilterArgs((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), NUM_TYPE_SIMPLE))
 
 
+# Fibonacci functions
+def fib(position):
+    if position <= 1:
+        return 1
+    return fib(position - 1) + fib(position - 2)
+
+
+# Fibonacci runner functions
+@run_time_decor
+def run_rib(position, renge=False):
+    if range:
+        fib_lists = []
+        for i in range(position):
+            fib_lists.append(fib(i))
+        return fib_lists
+    else:
+        return fib(position)
+
+
+print(run_rib(10, True))
+
+
+# Fibonacci runner functions generator
+@run_time_decor
+def fib_run_gen(position):
+    print('before generator')
+    for i in range(position):
+        print('in generate: ', i)
+        yield fib(i)
+
+
+print(list(fib_run_gen(20)))
+
+for fi in fib_run_gen(20):
+    if fi > 40:
+        print(fi)
+        print('fi great then 40')
+        break
