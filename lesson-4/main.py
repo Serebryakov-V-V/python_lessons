cb8cae54d124c0fe2e094426d7d36236f10d3d8a
Serebryakov-V-V <pro4web@yandex.ru>
2021-08-27 10:24:29 +0500

    fix homework

--- lesson-4/main.py
+++ lesson-4/main.py
@@ -1,4 +1,28 @@
-class product:
+class BaseProduct:
+    name_type = None
+
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+
+class Burger(BaseProduct):
+    name_type = 'Burger'
+
+    def __str__(self):
+        return f'{self.name_type} : {self.name} - {self.price}'
+
+
+class Pizza(BaseProduct):
+    name_type = 'Pizza'
+
+    def __init__(self, name, price, size):
+        super(self, name, price)
+        self.size = size
+
+product_1 = Pizza('Pizza', 250)
+product_2 = Burger('burger', 450)
+
+print(product_1.name, product_1.price)
+print(product_2)
+print(BaseProduct.name_type)
